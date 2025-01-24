8c950f9b93f185957473d4d783f21ad662744351
navid-kamal <navidkamal@iut-dhaka.edu>
2025-01-25 00:16:43 +0600

    build: initialize backend project with TypeScript and dependencies
    Set up the project structure with TypeScript configuration and installed required packages. This includes essential dependencies for development and runtime.

---
+++ server/server.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello000 World!');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
