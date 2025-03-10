7d06f569c06b30ffbbdba8734e1bd86830ee6f51
navid-kamal <navidkamal568@gmail.com>
2025-03-10 11:55:44 +0600

    feat(booking): implement booking service

--- server/modules/timeslot/timeslot.route.ts
+++ server/modules/timeslot/timeslot.route.ts
@@ -8,5 +8,6 @@ const timeslotController = new TimeSlotController();
 router.post('/generate', timeslotController.generateTimeSlot);
 router.get('/', timeslotController.getTimeSlot);
 router.get('/available/:turfId', timeslotController.getAvailableTimeSlot);
+router.put('/:id',timeslotController.updateTimeSlot)
 
 export default router;
