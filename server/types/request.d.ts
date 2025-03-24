c0c7fdc2d5f893bac99458870f5bd052102fa519
Kashshaf-Labib <labibkashshaf@gmail.com>
2025-03-25 01:21:18 +0600

    feat(filter interface): add interface in types for filtering

--- server/types/request.d.ts
+++ server/types/request.d.ts
@@ -16,3 +16,18 @@ export interface LoginBody {
   email: string;
   password: string;
 }
+
+export interface FilterOptions {
+  minPrice?: string;
+  maxPrice?: string;
+  teamSize?: string;
+  sports?: string | string[];
+  facilities?: string | string[];
+  preferredDay?: string;
+  preferredTime?: string;
+  latitude?: string;
+  longitude?: string;
+  radius?: string;
+  page?: string;
+  limit?: string;
+}
