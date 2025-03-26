ffaf14da67a84c32b36bcea1ea93fd1e778842cf
Kashshaf-Labib <labibkashshaf@gmail.com>
2025-03-27 00:34:56 +0600

    fix(maintainability issue): fix regex in utils/extractPublicIdFromUrl

--- server/utils/extractUrl.ts
+++ server/utils/extractUrl.ts
@@ -1,7 +1,7 @@
 export const extractPublicIdFromUrl = (url: string): string | null => {
   try {
-    const regex =
-      /cloudinary\.com\/[^\/]+\/image\/upload\/(?:v\d+\/)?(.+)(?:\.[^\.]+)$/; // Match the pattern: cloudinary.com/[cloud_name]/image/upload/v[version]/[public_id].[extension]
+    const regex = 
+      /cloudinary\.com\/[^/]+\/image\/upload\/(?:v\d+\/)?(.+)(?:\.[^.]+)$/; // Match the pattern: cloudinary.com/[cloud_name]/image/upload/v[version]/[public_id].[extension]
     const match = url.match(regex);
     return match ? match[1] : null;
   } catch (error) {
