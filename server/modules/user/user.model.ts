08ca9074c93be8e9173ac0eb91780f4ed8613b1a
navid-kamal <navidkamal568@gmail.com>
2025-04-12 11:17:14 +0600

    feat(user): enhance User model with global, organization, and event roles

--- server/modules/user/user.model.ts
+++ server/modules/user/user.model.ts
@@ -1,7 +1,9 @@
 import bcrypt from 'bcryptjs';
-import mongoose, { Document, Schema } from 'mongoose';
+import mongoose, { Types,Document, Schema } from 'mongoose';
 
-
+export interface IRoleAssignment {
+  role: Types.ObjectId; 
+}
 
 
 export interface UserDocument extends Document {
@@ -15,6 +17,15 @@ export interface UserDocument extends Document {
   verificationToken: string;
   verificationTokenExpires: Date;
   reviews: mongoose.Types.ObjectId[];
+  globalRoles: Types.ObjectId[];
+  organizationRoles: Array<{ 
+      organizationId: Types.ObjectId;
+      role: Types.ObjectId; 
+  }>;
+  eventRoles: Array<{ 
+      eventId: Types.ObjectId;
+      role: Types.ObjectId; 
+  }>;
 }
 
 const UserSchema = new Schema<UserDocument>(
@@ -27,7 +38,38 @@ const UserSchema = new Schema<UserDocument>(
     user_roles: { type: [String], required: true, default: ['user'] },
     isVerified: { type: Boolean, default: false }, // Default to false
     verificationToken: { type: String }, // Token for email verification
-    verificationTokenExpires: { type: Date }, // Token expiration time
+    verificationTokenExpires: { type: Date }, 
+    // Token expiration time
+    globalRoles: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Role',
+  }],
+  organizationRoles: [{
+      _id: false, // Don't create a separate _id for this subdocument array element
+      organizationId: {
+          type: Schema.Types.ObjectId,
+          ref: 'Organization', // Optional: Reference the Organization model
+          required: true,
+      },
+      role: {
+          type: Schema.Types.ObjectId,
+          ref: 'Role',
+          required: true,
+      }
+  }],
+  eventRoles: [{
+      _id: false,
+      eventId: {
+          type: Schema.Types.ObjectId,
+          // ref: 'Event', // Optional: Reference your Event model if you have one
+          required: true,
+      },
+      role: {
+          type: Schema.Types.ObjectId,
+          ref: 'Role',
+          required: true,
+      }
+  }],
     reviews: [{
       type: Schema.Types.ObjectId,
       ref: "TurfReview",
