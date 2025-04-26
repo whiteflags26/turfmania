d3242039123ec23541b4852a01444881985cdf07
whiteflags26 <whiteflags26@gmail.com>
2025-04-27 04:20:26 +0600

    feat(routes): enforce permission checks for facility, sports, and team size routes

--- server/modules/team_size/team_size.route.ts
+++ server/modules/team_size/team_size.route.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import TeamSizeController from "./team_size.controller";
+import { checkPermission, protect } from "../auth/auth.middleware";
+
 
 const router = Router();
 const teamSizeController = new TeamSizeController();
 
-router.post("/", teamSizeController.createTeamSize);
+router.post("/", checkPermission("manage_tags"), teamSizeController.createTeamSize);
 router.get("/", teamSizeController.getAllTeamSizes);
 router.get("/:id", teamSizeController.getTeamSizeById);
-router.put("/:id", teamSizeController.updateTeamSize);
-router.delete("/:id", teamSizeController.deleteTeamSize);
+router.put("/:id", checkPermission("manage_tags"), teamSizeController.updateTeamSize);
+router.delete("/:id", checkPermission("manage_tags"), teamSizeController.deleteTeamSize);
 
 export default router;
