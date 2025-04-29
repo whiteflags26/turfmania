b8a6afcd825f0b4c2da60af3d7e665260bf5ce56
navid-kamal <navidkamal568@gmail.com>
2025-04-29 12:16:32 +0600

    fixed(hydrationError)

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,23 +1,23 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import NavbarWrapper from "@/components/NavbarWrapper";
-import {AuthProvider} from '@/lib/contexts/authContext';
-import { Toaster } from "react-hot-toast";
+import NavbarWrapper from '@/components/NavbarWrapper';
+import { AuthProvider } from '@/lib/contexts/authContext';
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import { Toaster } from 'react-hot-toast';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "TurfMania",
-  description: "Streamlining the Turf Industry in Bangladesh",
+  title: 'TurfMania',
+  description: 'Streamlining the Turf Industry in Bangladesh',
 };
 
 export default function RootLayout({
@@ -31,9 +31,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-        <NavbarWrapper />
-        {children}
-        <Toaster position="top-center" />
+          <main>
+            <NavbarWrapper />
+            {children}
+          </main>
+          <Toaster position="top-center" />
         </AuthProvider>
       </body>
     </html>
